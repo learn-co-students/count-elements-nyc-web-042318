0618fa2b2460e9781ec1b89fe4f7799320af76f5
blevm <mjblevins7@gmail.com>
2018-04-23 19:50:21 -0400

    mb solution

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
-  # code goes here
+  array_with_count = {}
+
+  array.each do |item|
+    if array_with_count.keys.include?(item)
+      array_with_count[item] += 1
+    else
+      array_with_count[item] = 1
+    end
+  end
+  array_with_count
 end
- 
