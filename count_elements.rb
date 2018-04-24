10602c127664f544ce163f7c121add3c644f6543
Mendy <mendywoly@gmail.com>
2018-04-24 08:46:59 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,16 @@
+require "pry"
 def count_elements(array)
   # code goes here
+  hash = {}
+
+  array.each do |i|
+      if  hash.keys.include?(i)
+      hash[i] += 1
+    else
+      hash[i] = 1
+    end
+  end
+  hash
 end
- 
+
+hash = {"mendy" => 1}
