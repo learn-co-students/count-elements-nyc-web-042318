73523966e0d46d916d153983b4ade0e42e8b9b19
derek chen <derek.g.chen@gmail.com>
2018-04-25 00:59:01 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,15 @@
+require 'pry'
 def count_elements(array)
   # code goes here
+  result = {}
+
+  array.each do |value|
+    if result.has_key?(value)
+      result[value] += 1
+    else
+      result[value] = 1
+    end
+  end
+  result
+
 end
- 
